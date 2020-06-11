5add7fe60e5853db337b80e9f4f766acab5079c8
Thomas Crilly <n9960783@qut.edu.au>
2020-06-11 13:00:39 +1000

    Implement un-auth stock queries

--- routes/index.js
+++ routes/index.js
@@ -7,30 +7,48 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
-router.get("/stocks", function(req, res, next) {
-	// res.render('index', {title: "The /stocks/ route"});
-	
-	req.db.from('stocks').distinct('name', 'symbol', 'industry')
-		.then((rows) => {
-			res.json({"Error" : false, "Message" : "Success", "Stocks" : rows})
-		})
-		.catch((err) => {
-			console.log(err);
-			res.json({"Error" : true, "Message" : "Error in MySQL Query"})
-		});
-
-
-});
-
 router.get("/stocks/symbols", function(req, res, next) {
-	// parse request - filter & add selection to sql query
-	// if request == /stocks/symbols ..........
-	// if request == /stocks/_____ then () => {}
-	res.render('index', {title: "The /stocks/symbols route"});
+	// If industry query supplied, select one entry for each distinct
+	// where industry == industry
+	if (req.query.industry) {
+		req.db.from('stocks').distinct('name', 'symbol', 'industry')
+			.where('industry', '=', req.query.industry)
+			.then((rows) => {
+				res.json({"Error" : false, "Message" : "Success", "Stocks" : rows})
+			})
+			.catch((err) => {
+				res.json({"Error" : true, "Message" : "Error executing MySQL Query"})
+			});
+	}
+	// Else if no query supplied, return one entry for each distinct name, symbol, and industry
+	else {
+		req.db.from('stocks').distinct('name', 'symbol', 'industry')
+			.then((rows) => {
+				res.json({"Error" : false, "Message" : "Success", "Stocks" : rows})
+			})
+			.catch((err) => {
+				res.json({"Error" : true, "Message" : "Error executing MySQL Query"})
+			});
+	}
+	
 });
 
 router.get("/stocks/authed/", function(req, res, next) {
 	res.render('index', {title: "The /stocks/authed/ route"});
+});
+
+router.get("/stocks/:StockSymbol", function(req, res, next) {	// MUST HAVE STOCK SYMBOL OR ERROR
+	// RETURNS SINGLE STOCK FROM LATEST TIMESTAMP
+	req.db.from('stocks').select('*').where('Symbol', '=', req.params.StockSymbol)
+		.where('timestamp', '=',
+			req.db.from('stocks').where('Symbol', '=', req.params.StockSymbol)
+				.max('timestamp'))
+		.then((rows) => {
+			res.json({"Error" : false, "Message" : "Success", "Stocks" : rows})
+		})
+		.catch((err) => {
+			res.json({"Error" : true, "Message" : "Error executing MySQL Query"})
+		});
 });
 
 // router.get("/")
